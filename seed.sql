bc6d8ba9388e0126522a2af1760999a3747bd5f0
Christian Sinai <christian_sinai@me.com>
2020-10-04 07:47:44 -0700

    created seperate sql seed file

--- seed.sql
+++ seed.sql
@@ -1,38 +1,42 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+-- SEED DATA
+INSERT INTO department (id, name) VALUES (1, 'Engineering');
+INSERT INTO role (id, title, salary, department_id) VALUES (1, 'Software Engineer', 100000, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'Christian', 'Sinai', 1, NULL);
 
-USE employee_db;
+/* EXAMPLE SEED CODE
+INSERT INTO department (branch) VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
 
-CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+INSERT INTO job (title, salary, department_id)
+VALUES ("Sales Lead", 100000, (SELECT id FROM department where branch = "Sales")),
+("Salesperson", 80000, (SELECT id FROM department where branch = "Sales")),
+("Lead Engineer", 150000, (SELECT id FROM department where branch = "Engineering")),
+("Software Engineer", 120000, (SELECT id FROM department where branch = "Engineering")),
+("Accountant", 125000, (SELECT id FROM department where branch = "Finance")),
+("Legal Team Lead", 100000,(SELECT id FROM department where branch = "Legal")),
+("Lawyer", 100000, (SELECT id FROM department where branch = "Finance"));
 
-/*
-QUESTION: is it OK to add insert statement here, or best after all create statements?
-QUESTION: how many lines of 'seed' date is needed/ best?
-QUESTION: what's best for dummy data?
+INSERT INTO employee (first_name, last_name, job_id)
+VALUES ("John", "Doe", (SELECT id FROM job where title = "Sales Lead")),
+("Mike", "Chan",(SELECT id FROM job where title = "Salesperson")),
+("Ashley", "Rodriguez",(SELECT id FROM job where title = "Lead Engineer")),
+("Kevin", "Tupik",(SELECT id FROM job where title = "Software Engineer")),
+("Malia", "Brown",(SELECT id FROM job where title = "Accountant")),
+("Sarah", "Lourd",(SELECT id FROM job where title = "Legal Team Lead")),
+("Tom", "Allen",(SELECT id FROM job where title = "Lawyer"));
+
+UPDATE employee
+SET manager_id = 3
+WHERE id = 1;
+
+UPDATE employee
+SET manager_id = 3
+WHERE id = 4;
+
+UPDATE employee
+SET manager_id = 6
+WHERE id = 7;
+
+UPDATE employee
+SET manager_id = 1
+WHERE id = 2;
 */
-INSERT INTO department (id, name) VALUES (1, 'department');
-
-CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2),
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO role (id, title, salary, department_id) VALUES (1, 'title', 0, 1);
-
-CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NULL,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'first_name', 'last_name', 1, 1);
