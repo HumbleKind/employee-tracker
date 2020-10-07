ea565edc0573a13524ac6e4794df811e18744ea9
Christian Sinai <christian_sinai@me.com>
2020-10-06 21:28:15 -0700

    updated seed data to matach assignment example

--- seed.sql
+++ seed.sql
@@ -1,42 +1,22 @@
 -- SEED DATA
-INSERT INTO department (id, name) VALUES (1, 'Engineering');
-INSERT INTO role (id, title, salary, department_id) VALUES (1, 'Software Engineer', 100000, 1);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, 'Christian', 'Sinai', 1, NULL);
+INSERT INTO department (name)
+VALUES ("Engineering"),("Finance"),("Legal"),("Sales");
 
-/* EXAMPLE SEED CODE
-INSERT INTO department (branch) VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 2),
+("Lawyer", 100000, 3),
+("Lead Engineer", 150000, 1),
+("Legal Team Lead", 100000, 3),
+("Sales Lead", 100000, 4),
+("Salesperson", 80000, 4),
+("Software Engineer", 120000, 1);
 
-INSERT INTO job (title, salary, department_id)
-VALUES ("Sales Lead", 100000, (SELECT id FROM department where branch = "Sales")),
-("Salesperson", 80000, (SELECT id FROM department where branch = "Sales")),
-("Lead Engineer", 150000, (SELECT id FROM department where branch = "Engineering")),
-("Software Engineer", 120000, (SELECT id FROM department where branch = "Engineering")),
-("Accountant", 125000, (SELECT id FROM department where branch = "Finance")),
-("Legal Team Lead", 100000,(SELECT id FROM department where branch = "Legal")),
-("Lawyer", 100000, (SELECT id FROM department where branch = "Finance"));
-
-INSERT INTO employee (first_name, last_name, job_id)
-VALUES ("John", "Doe", (SELECT id FROM job where title = "Sales Lead")),
-("Mike", "Chan",(SELECT id FROM job where title = "Salesperson")),
-("Ashley", "Rodriguez",(SELECT id FROM job where title = "Lead Engineer")),
-("Kevin", "Tupik",(SELECT id FROM job where title = "Software Engineer")),
-("Malia", "Brown",(SELECT id FROM job where title = "Accountant")),
-("Sarah", "Lourd",(SELECT id FROM job where title = "Legal Team Lead")),
-("Tom", "Allen",(SELECT id FROM job where title = "Lawyer"));
-
-UPDATE employee
-SET manager_id = 3
-WHERE id = 1;
-
-UPDATE employee
-SET manager_id = 3
-WHERE id = 4;
-
-UPDATE employee
-SET manager_id = 6
-WHERE id = 7;
-
-UPDATE employee
-SET manager_id = 1
-WHERE id = 2;
-*/
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 5, 3),
+("Mike", "Chan", 6, 1),
+("Ashley", "Rodriguez", 3, null),
+("Kevin", "Tupik", 7, 3),
+("Malia", "Brown", 1, null),
+("Sarah", "Lourd", 4, null),
+("Tom", "Allen", 2, 6),
+("Christian", "Eckenrode", 3, 2);
