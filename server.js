fc501b47482af3eee9bd94d5198cc90f013f2fa4
Christian Sinai <christian_sinai@me.com>
2020-10-07 07:45:28 -0700

    added view all roles method

--- server.js
+++ server.js
@@ -136,3 +136,12 @@ function addEmployee() {
             })
         });
 }
+
+function viewRoles() {
+    var query = "SELECT * FROM role";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        runTask();
+    })
+}
